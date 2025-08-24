462154ca00af880b00ba3d6861109d1ed443369e
vishnureddy <tarigopula34@gmail.com>
2025-08-24 16:29:03 +0530

    terraform roboshop project dev and qa env

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "sg" {
-  name        = "#{var.component_name}-${var.env}-sg"
+  name        = "${var.component_name}-${var.env}-sg"
   description = "inbound allow for ${var.component_name}"
 
   ingress {
@@ -26,7 +26,7 @@ resource "aws_security_group" "sg" {
 }resource "aws_instance" "instance" {
   ami           = "data.aws_ami.ami.id"
   instance_type = "var.instance_type"
-  vpc_security_group_ids = ["aws_security_groups.sg.id"]
+  vpc_security_group_ids = ["aws_security_group.sg.id"]
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
