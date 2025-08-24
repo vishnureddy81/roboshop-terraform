0c47a839a98325f32106c3946112444094439ee9
vishnureddy <tarigopula34@gmail.com>
2025-08-24 16:19:23 +0530

    terraform roboshop project dev and qa env

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -25,14 +25,10 @@ resource "aws_security_group" "sg" {
   }
 
 
-
-
-
-
 }resource "aws_instance" "instance" {
   ami           = "data.aws_ami.ami.id"
   instance_type = "var.instance_type"
-  vpc_security_group_ids = ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["aws_security_groups.sg.id"]
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
