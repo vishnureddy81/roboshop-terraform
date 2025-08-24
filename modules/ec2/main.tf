3feec3fa999e1f69032260d44d0b755ed6405bbb
vishnureddy <tarigopula34@gmail.com>
2025-08-24 17:40:28 +0530

    terraform roboshop project dev and qa env

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,37 +3,33 @@ resource "aws_security_group" "sg" {
   description = "inbound allow for ${var.component_name}"
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port        = var.app_port
-    to_port          = var.app_port
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
 
 resource "aws_instance" "instance" {
-  ami           = "data.aws_ami.ami.id"
-  instance_type = "var.instance_type"
-  vpc_security_group_ids = ["aws_security_group.sg.id"]
+  ami                    = data.aws_ami.ami.id   # ✅ fixed
+  instance_type          = var.instance_type     # ✅ fixed
+  vpc_security_group_ids = [aws_security_group.sg.id]  # ✅ fixed
+
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
 }
-
-
-
-#
