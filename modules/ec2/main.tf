4d3dad79794ea797450aab0195917ec89d21561a
vishnureddy <tarigopula34@gmail.com>
2025-08-25 10:49:26 +0530

    terraform roboshop project dev and qa env

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -34,16 +34,23 @@ resource "aws_instance" "instance" {
   }
 
 
-
-
-provisioner "local-exec" {
-    command = <<EOL
-cd /home/ec2-user/roboshop-ansible
-ansible-playbook -i ${self.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} main.yml
-EOL
+  provisioner "remote-exec" {}
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.private_ip
   }
 
-}
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/vishnureddy81/roboshop-ansible roboshop.yml -e env=${var.env} -e app_name=${component_name}"
+    ]
+
+  }
+
+
+
 
 
 
